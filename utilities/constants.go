5fae3c17eea4ea58adecbe65fe141399a5a46e9a
kind2511 <chrisa2511@gmail.com>
2024-02-17 13:06:03 +0100

    updated the endpoints

--- utilities/constants.go
+++ utilities/constants.go
@@ -1,7 +1,7 @@
 package utilities
 
 // URL PATHS
-const BOOKCOUNT_PATH = "/bookcount"
-const READERSHIP_PATH = "/readership"
-const STATUS_PATH = "/status"
+const BOOKCOUNT_PATH = "/librarystats/v1/bookcount/"
+const READERSHIP_PATH = "/librarystats/v1/readership/"
+const STATUS_PATH = "/librarystats/v1/status/"
 
