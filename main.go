c50947871ffad73e25d4fed503368c9d78fe3858
kind2511 <chrisa2511@gmail.com>
2024-02-17 12:54:15 +0100

    port and server
    handled port assignment as well as created a basic webserver

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World")
+
+	// Assign Port
+	port:= os.Getenv("PORT");
+
+	if port == "" {
+		log.Println("The PORT has not been set. Default: 8080")
+		port = "8080"
+	}
+
+	// Start http Server
+	log.Println("Starting server on port " + port + "...")
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
+
