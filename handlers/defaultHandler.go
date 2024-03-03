dea842cd78fc525f11a0450b439d88a6e8dd7562
kind2511 <chrisa2511@gmail.com>
2024-03-03 11:23:29 +0100

    mistake in defaulthandler

--- handlers/defaultHandler.go
+++ handlers/defaultHandler.go
@@ -6,5 +6,5 @@ import (
 )
 
 func DefaultHandler(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "This service does not provide any functionality on root path level.\n\nPlease use paths:\n"+utilities.BOOKCOUNT_PATH+"?language={:two_letter_language_code+}/, \n"+utilities.READERSHIP_PATH+"{:two_letter_language_code}{?limit={:number}}/, \n"+utilities.STATUS_PATH, http.StatusOK)
+	http.Error(w, "This service does not provide any functionality on root path level.\n\nPlease use paths:\n"+utilities.BOOKCOUNT_PATH+"{:two_letter_language_code+}/, \n"+utilities.READERSHIP_PATH+"{:two_letter_language_code}{?limit={:number}}/, \n"+utilities.STATUS_PATH, http.StatusOK)
 }
